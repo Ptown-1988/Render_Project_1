cfdbaff5c7f99441c5f75beb8f351be8cd913591
Ptown-1988 <builtonsolidrock88@gmail.com>
2025-04-13 19:15:27 -0400

    Add Widgets Final2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if 'df_experiment_results' not in st.session_state:
 
 st.header('Tossing a Coin')
 
-chart = st.line_chart([0.5])
+chart = st.line_chart([0.5]).astype(float)
 
 def toss_coin(n):
 
